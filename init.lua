1f5f5bd43af8f882cf3816ad9d17166e009138a1
Josef Bauer <josef.bauer.1st@gmail.com>
2024-02-01 10:39:41 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,32 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+vim.opt.tabstop = 4
+vim.opt.relativenumber = true
+vim.opt.whichwrap = "<>[]"
+
+vim.filetype.add({ extension = { templ = "templ" }})
+local custom_format = function()
+    if vim.bo.filetype == "templ" then
+        local bufnr = vim.api.nvim_get_current_buf()
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+        vim.fn.jobstart(cmd, {
+            on_exit = function()
+                -- Reload the buffer only if it's still the current buffer
+                if vim.api.nvim_get_current_buf() == bufnr then
+                    vim.cmd('e!')
+                end
+            end,
+        })
+    else
+        vim.lsp.buf.format()
+    end
+end
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = custom_format })
